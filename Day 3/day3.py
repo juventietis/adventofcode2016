212d4108a3d808ca4d4be8c117ba1b84c8485c20
juventietis <mantasmarkeviciuslt@gmail.com>
2017-02-26 20:36:30 +0000

    Day3 Part2 solution

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -8,15 +8,27 @@ def is_triangle(a, b, c):
         return False
 
 
+def solve_p1(data):
+    triangles = 0
+    for line in input_data:
+        triangle = [int(x) for x in line.split()]
+        if is_triangle(triangle[0], triangle[1], triangle[2]):
+            triangles += 1
+    print(triangles)
+
+
+def solve_p2(data):
+    matrix = [[int(x) for x in line.split()] for line in data]
+    triangles = 0
+    for i in range(0, len(matrix), 3):
+        for o in range(3):
+            if is_triangle(matrix[i][o], matrix[i+1][o], matrix[i+2][o]):
+                triangles += 1
+    print (triangles)
+
 input_data = None
 with open("input.dat") as data:
     input_data = data.readlines()
 
-
-triangles = 0
-for line in input_data:
-    triangle = [int(x) for x in line.split()]
-    if is_triangle(triangle[0], triangle[1], triangle[2]):
-        triangles += 1
-
-print (triangles)
+solve_p1(input_data)
+solve_p2(input_data)
